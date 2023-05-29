89368edf66e7d649525cfb8114ffc63cde311a8b
Polemeest <118891802+polemeest@users.noreply.github.com>
2023-05-29 14:24:26 +0400

    5 day edit

--- 4kyu_codewars_sum_of_intervals.py
+++ 4kyu_codewars_sum_of_intervals.py
@@ -1,21 +1,5 @@
 import time
 
-# def check(intervals, log):
-#     while intervals:
-#         current = intervals.pop(0)
-#         for i, item in enumerate(log):
-#             if current[0] in range(*item) and current[1] in range(*item):
-#                 break
-#             elif current[0] in range(*item) and current[1] > item[1]:
-#                 log[i][0] = current[1]
-#                 break
-#             elif current[0] < item[0] and current[1] in range(*item):
-#                 log[i][1] = current[0]
-#                 break
-#         else:
-#             log.append(list(current))
-#     return log
-
 
 def sum_of_intervals(intervals):
     start = time.time()
@@ -33,14 +17,46 @@ def sum_of_intervals(intervals):
     return sum([len(range(*item)) for item in set(log)]), end
 
 
-
-
-#     return sum(i[2] for i in a), b
-#     # [b-a for a, b in item]
-#   #  print([[b - a for a, b in item] for item in intervals])
 print(sum_of_intervals([(483, 490), (315, 498), (306, 307)]))
 print(sum_of_intervals([(268, 462), (358, 484), (187, 369), (-430, -353), (-341, 411), (433, 485)]))
 
+'''
+Not mine solutions:
+
+
+def sum_of_intervals(intervals):
+    intervals.sort()
+    lim,res=intervals[0][0],0
+    for i in range(len(intervals)):
+        res+=max(intervals[i][1],lim)-max(intervals[i][0],lim)
+        lim=max(lim,intervals[i][1])
+    return res
+
+
+def sum_of_intervals(intervals):
+    s, top = 0, float("-inf")
+    for a,b in sorted(intervals):
+        if top < a: top    = a
+        if top < b: s, top = s+b-top, b
+    return s
+'''
+
+''' Mine solutions: '''
+# def check(intervals, log):
+#     while intervals:
+#         current = intervals.pop(0)
+#         for i, item in enumerate(log):
+#             if current[0] in range(*item) and current[1] in range(*item):
+#                 break
+#             elif current[0] in range(*item) and current[1] > item[1]:
+#                 log[i][0] = current[1]
+#                 break
+#             elif current[0] < item[0] and current[1] in range(*item):
+#                 log[i][1] = current[0]
+#                 break
+#         else:
+#             log.append(list(current))
+#     return log
 
 #    # b = [intervals[::-1].pop()]
 #    # log = []
