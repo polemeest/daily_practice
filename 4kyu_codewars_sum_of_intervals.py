6ab21f49b6f75dfc5a3a3e1d594ced38f0589208
Polemeest <118891802+polemeest@users.noreply.github.com>
2023-05-29 14:15:18 +0400

    5 day

---
+++ 4kyu_codewars_sum_of_intervals.py
@@ -0,0 +1,84 @@
+import time
+
+# def check(intervals, log):
+#     while intervals:
+#         current = intervals.pop(0)
+#         for i, item in enumerate(log):
+#             if current[0] in range(*item) and current[1] in range(*item):
+#                 break
+#             elif current[0] in range(*item) and current[1] > item[1]:
+#                 log[i][0] = current[1]
+#                 break
+#             elif current[0] < item[0] and current[1] in range(*item):
+#                 log[i][1] = current[0]
+#                 break
+#         else:
+#             log.append(list(current))
+#     return log
+
+
+def sum_of_intervals(intervals):
+    start = time.time()
+    intervals.sort()
+    log = [intervals.pop(0)]
+    while intervals:
+        current = intervals.pop(0)
+        for i, item in enumerate(log):
+            if current[0] <= item[1]:
+                log[i] = (item[0], max(item[1], current[1]))
+                break
+        else:
+            log.append(current)
+    end = time.time() - start
+    return sum([len(range(*item)) for item in set(log)]), end
+
+
+
+
+#     return sum(i[2] for i in a), b
+#     # [b-a for a, b in item]
+#   #  print([[b - a for a, b in item] for item in intervals])
+print(sum_of_intervals([(483, 490), (315, 498), (306, 307)]))
+print(sum_of_intervals([(268, 462), (358, 484), (187, 369), (-430, -353), (-341, 411), (433, 485)]))
+
+
+#    # b = [intervals[::-1].pop()]
+#    # log = []
+#    # print(b)
+#    # for i, item in enumerate(intervals):
+#    #     # if not b:
+#    #     #     b.append(item[0], item[1])
+#    #     if item[0] in range(*b[len(b) - 1]) or item[0] < b[len(b) - 1][0] and item[1] > b[len(b) - 1][1]:
+#    #         b[len(b) - 1] = [min(b[len(b) - 1][0], item[0]), max(b[len(b) - 1][1], item[1])]
+#    #     else:
+#    #         b.append((item[0], item[1]))
+#
+#
+#    # return b
+#
+#
+#    # [[log.append(i) for i in range(*item) if i not in log] for item in intervals]
+#    # return len(log)
+#
+#
+#    #     start = time.time()
+#    # log = [intervals.pop(0)]
+#    # # log = check(intervals, log)
+#    # # ends = check(log, [[0,0]])
+#    # while intervals:
+#    #     current = intervals.pop(0)
+#    #     for i, item in enumerate(log):
+#    #         if current[0] in range(*item) and current[1] in range(*item):
+#    #             break
+#    #         elif (current[0] < item[0] and current[1] in range(*item)) or (current[0] in range(*item) and current[1] > item[1]):
+#    #             log[i] = (min(current[0], item[0]), max(current[1], item[1]))
+#    #             break
+#    #         elif current[0] <= item[0] and current[1] >= item[1]:
+#    #             log[i] = item
+#    #     else:
+#    #         log.append(current)
+#    # logs = {s for s in log if log.count(s) == 1}
+#    # print(log)
+#    # print(logs)
+#    # end = time.time() - start
+#    # return sum([len(range(*item)) for item in set(logs)]), end
