adce3b44fcc2749a8cb8a6ef9139d3ab0bf92ef2
Polemeest <118891802+polemeest@users.noreply.github.com>
2023-06-23 17:50:00 +0400

    18 day ++

--- hackerrank_door_mat_manufacturing.py
+++ hackerrank_door_mat_manufacturing.py
@@ -1,3 +1,24 @@
 def print_door_mat(height: int, width: int) -> None:
-    for i in range(height):
-        print(f'')
+    half = height // 2
+    log = []
+    for i in range(half):
+        log.append(f'{".|." * (1 + 2 * i):-^{width}}')
+    log.append(f'{"WELCOME":-^{width}}')
+    log += reversed(log[:-1])
+    for i in log:
+        print(i)
+
+
+
+
+    # for i in range(height):
+    #     if i < half:
+    #         print(f'{".|." * (1 + 2 * i):-^{width}}')
+    #     elif i == half:
+    #         print(f'{"WELCOME":-^{width}}')
+    #     else:
+    #         print(f'{".|." * (height - 2 * (i % (half + 1))):-^{width}}')
+
+
+
+print_door_mat(11, 33)
