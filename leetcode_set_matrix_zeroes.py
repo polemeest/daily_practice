0b913e40d8308cc86d42fc130fc3957258f038b5
polemeest <doran6112@gmail.com>
2024-03-01 16:10:01 +0400

    01.03.24

--- leetcode_set_matrix_zeroes.py
+++ leetcode_set_matrix_zeroes.py
@@ -4,20 +4,25 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        coordinates = set()
-        for row, v in enumerate(matrix):
-            for col, vv in enumerate(v):
-                if vv == 0:
-                    coordinates.add((row, col))
-        for item in coordinates:
-            row, col = item
-            for i, v in enumerate(matrix[row]):
-                matrix[row][i] = 0
-            for i, v in enumerate(matrix):
-                matrix[i][col] = 0
+        # coordinates = set()
+        # for row, v in enumerate(matrix):
+        #     for col, vv in enumerate(v):
+        #         if vv == 0:
+        #             coordinates.add((row, col))
+        # for item in coordinates:
+        #     row, col = item
+        #     for i, v in enumerate(matrix[row]):
+        #         matrix[row][i] = 0
+        #     for i, v in enumerate(matrix):
+        #         matrix[i][col] = 0
 
-        print(matrix)
+        # print(matrix)
+
 
 
 a = Solution()
-a.setZeroes(matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]])
+a.setZeroes(matrix = [   [0,1,2,0],
+                         [3,4,5,2],
+                         [1,3,1,5],
+                         [1,3,1,5],
+                    ])
---
+++ leetcode_transpose_matrix.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        my = [[0] * len(matrix) for i in range(len(matrix[0]))]
+
+        for i, v in enumerate(my):
+            for j, vv in enumerate(v):
+                my[j][i] = matrix[i][j]
+        return my
+    
+
+a = Solution()
+print(a.transpose([[1,2,3],[4,5,6]]))
